0420d681996304ef24fd25834997222d24ad7754
Artypuppet <jaspreetsingh8437055402@gmail.com>
2024-01-20 17:37:51 -0500

    Schedule.py nearly ended schedule method

--- Scheduler.py
+++ Scheduler.py
@@ -13,7 +13,10 @@ class Scheduler(Subject):
     def schedule(self):
         self.csvFile.sort()
         for row in self.csvFile:
-            row
+            year, month, day = row[0].split(' ')[0].split("-")
+            date = date(year, month, day)
+            if date in self.days:
+                self.days[date].setAppt(row[2], row[1])
 
     def sortCSVFileByRequestTime(self, low: int, high: int) -> None:
         if (low >= high):
@@ -31,20 +34,20 @@ class Scheduler(Subject):
         k = low
         while i < len(left) and j < len(right):
             if (self.compareRequestTime(left[0][i], right[0][j])):
-                self.csvFile.iloc[k] = left[i]
+                self.csvFile.iloc[k] = left[i].copy()
                 k += 1
                 i += 1
             else:
-                self.csvFile.iloc[k] = right[j]
+                self.csvFile.iloc[k] = right[j].copy()
                 k += 1
                 j += 1
         while i < len(left):
-            self.csvFile.iloc[k] = left[i]
+            self.csvFile.iloc[k] = left[i].copy()
             k += 1
             i += 1
 
         while j < len(right):
-            self.csvFile.iloc[k] = right[j]
+            self.csvFile.iloc[k] = right[j].copy()
             k += 1
             j += 1
 
