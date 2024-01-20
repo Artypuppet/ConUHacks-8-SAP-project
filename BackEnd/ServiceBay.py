c16a7c904a2b0d966646571b78fd0df479258f58
xdavidg <157243142+xdavidg@users.noreply.github.com>
2024-01-20 16:06:25 -0500

    Update ServiceBay.py

--- BackEnd/ServiceBay.py
+++ BackEnd/ServiceBay.py
@@ -1,5 +1,17 @@
 import datetime as dt
+import Appointment
+import Day
 
 
 class ServiceBay:
-    def __init__(self, day: dt.date)
+    def __init__(self, day: dt.date, appt: 'Appointment'):
+        self.appt = []
+        self.day = day
+
+    def isOccupied(self, newAppt: 'Appointment') -> (bool, str):
+        for existingAppt in self.appt:
+            if (newAppt.appt_start < existingAppt.appt_end and 
+                newAppt.appt_end > existingAppt.appt_start):
+                return True, existingAppt.car_type
+        return False, None
+        
