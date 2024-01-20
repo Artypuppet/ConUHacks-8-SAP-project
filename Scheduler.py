a1d7edbf7cbee8ef7794020ba2e6efbe5c0e9498
Artypuppet <jaspreetsingh8437055402@gmail.com>
2024-01-20 17:25:43 -0500

    another Scheduler update

--- Scheduler.py
+++ Scheduler.py
@@ -1,37 +1,36 @@
 from Subject import Subject
 from Day import Day
-from StatisticalTracker import StatisticalTracker
 import pandas as pd
 from datetime import date
 
+
 class Scheduler(Subject):
 
-    def __init__(self, csvFileName: str, statTracker: StatisticalTracker):
+    def __init__(self, csvFileName: str):
         self.days = {}
         self.csvFile = pd.read_csv(csvFileName)
-        self.statTracker = StatisticalTracker()
-        
 
-    def schedule(self, appointment):
+    def schedule(self):
         self.csvFile.sort()
         for row in self.csvFile:
-            row 
-    def sortCSVFileByRequestTime(self, low: int, high: int)-> None:
-        if(low >= high):
+            row
+
+    def sortCSVFileByRequestTime(self, low: int, high: int) -> None:
+        if (low >= high):
             return None
         middle = low + high / 2
         self.sortCSVFileByRequestTime(low, middle)
         self.sortCSVFileByRequestTime(middle + 1, high)
         self.merge(low, middle, high)
-    
+
     def merge(self, low: int, middle: int, high: int):
         left = self.csvFile.loc[low: middle]
-        right = self.csvFile.loc[middle + 1: high]  
-        i = low 
+        right = self.csvFile.loc[middle + 1: high]
+        i = low
         j = middle + 1
         k = low
         while i < len(left) and j < len(right):
-            if(self.compareRequestTime(left[0][i], right[0][j]))
+            if (self.compareRequestTime(left[0][i], right[0][j])):
                 self.csvFile.iloc[k] = left[i]
                 k += 1
                 i += 1
@@ -43,17 +42,15 @@ class Scheduler(Subject):
             self.csvFile.iloc[k] = left[i]
             k += 1
             i += 1
-        
+
         while j < len(right):
             self.csvFile.iloc[k] = right[j]
             k += 1
             j += 1
 
-        
-    def compareRequestTime(self, date1:str, date2: str):
+    def compareRequestTime(self, date1: str, date2: str):
         year, month, day = date1.split(' ')[0].split("-")
         dateObj1 = date(int(year), int(month), int(day))
         year, month, day = date2.split(' ')[0].split("-")
         dateObj2 = date(int(year), int(month), int(day))
         return dateObj1 < dateObj2
-        
