e7ec5f2bea18d8cc841726ce282b1d9344cfcb4d
xdavidg <157243142+xdavidg@users.noreply.github.com>
2024-01-20 21:56:00 -0500

    Update ServiceBay.py

--- BackEnd/ServiceBay.py
+++ BackEnd/ServiceBay.py
@@ -1,6 +1,6 @@
 import datetime as dt
+import bisect
 from Appointment import Appointments
-import Day
 
 
 class ServiceBay:
@@ -8,17 +8,13 @@ class ServiceBay:
         self.appt = []
         self.day = day
 
-    def isOccupied(self, newAppt: Appointments) -> (bool, str):
+    def addAppointment(self, new_appt: Appointments):
+        position = bisect.bisect_left(self.appt, new_appt)
+        self.appt.insert(position, new_appt)
+
+    def balanceOfCarType(self, Appt: Appointments):
         for existingAppt in self.appt:
-            if (newAppt.appt_start >= existingAppt.appt_end or
-                    newAppt.appt_end <= existingAppt.appt_start):
-                return True, existingAppt.car_type
-        return False, None
-
-
-def balanceOfCarType(self, Appt: Appointments):
-    for existingAppt in self.appt:
-        if (Appt.appt_start < existingAppt.appt_end or
-                Appt.appt_end > existingAppt.appt_start):
-            return existingAppt.car_type
-        return 'Empty'
+            if (Appt.appt_start < existingAppt.appt_end or
+                    Appt.appt_end > existingAppt.appt_start):
+                return existingAppt.car_type
+            return 'Empty'
