557cf79f09b06e7ed769a42e740efae85c556a69
Artypuppet <jaspreetsingh8437055402@gmail.com>
2024-01-20 20:35:45 -0500

    scheduler works

--- Scheduler.py
+++ Scheduler.py
@@ -1,27 +1,39 @@
 from Subject import Subject
 from Day import Day
 import pandas as pd
+from datetime import datetime
 from datetime import date
 from Appointment import Appointments
+import csv
 
 
 class Scheduler(Subject):
 
     def __init__(self, csvFileName: str):
         self.days = {}
-        self.csvFile = pd.read_csv(csvFileName)
+        self.csvFile = []
+        self.loadFile(csvFileName)
+
+    def loadFile(self, csvFileName):
+        with open(csvFileName, "r") as file:
+            next(file)
+            reader = csv.reader(file)
+            self.csvFile = []
+            for row in reader:
+                self.csvFile.append(row)
 
     def schedule(self):
         self.sortCSVFileByRequestTime(0, len(self.csvFile) - 1)
-        self.csvFile.to_csv("output.csv")
-        for ind in self.csvFile.index:
-            year, month, day = self.csvFile.loc[ind, "Appointment"].split(' ')[0].split(
+        print(len(self.csvFile))
+        print(self.csvFile)
+        for ind in range(len(self.csvFile)):
+            year, month, day = self.csvFile[ind][0].split(' ')[0].split(
                 "-")
             apptDate = date(int(year), int(month), int(day))
             if apptDate not in self.days:
                 self.days[apptDate] = Day(apptDate)
             appointment = Appointments(
-                self.csvFile["Appointment"][ind], self.csvFile["CarType"][ind])
+                self.csvFile[1][ind], self.csvFile[2][ind])
             if (self.days[apptDate].add_appointment(appointment)):
                 appointment.status = 'Success'
                 self.Notify(appointment)
@@ -37,33 +49,40 @@ class Scheduler(Subject):
         self.merge(low, middle, high)
 
     def merge(self, low: int, middle: int, high: int):
-        left = self.csvFile.loc[low: middle]
-        right = self.csvFile.loc[middle + 1: high]
-        i = low
-        j = middle + 1
+        left = self.csvFile[low: middle + 1]
+        right = self.csvFile[middle + 1: high]
+        i = 0
+        j = 0
         k = low
         while i < len(left) and j < len(right):
-            if (self.compareRequestTime(left[0][i], right[0][j])):
-                self.csvFile.iloc[k] = left.iloc[i].copy()
-                k += 1
+            l = left[i][0]
+            r = right[j][0]
+            if (self.compareRequestTime(l, r)):
+                self.csvFile[k] = left[i]
                 i += 1
             else:
-                self.csvFile.iloc[k] = right.iloc[j].copy()
-                k += 1
+                self.csvFile[k] = right[j]
                 j += 1
+            k += 1
         while i < len(left):
-            self.csvFile.iloc[k] = left.iloc[i].copy()
+            self.csvFile[k] = left[i]
             k += 1
             i += 1
 
         while j < len(right):
-            self.csvFile.iloc[k] = right.iloc[j].copy()
+            self.csvFile[k] = right[j]
             k += 1
             j += 1
 
     def compareRequestTime(self, date1: str, date2: str):
-        year, month, day = date1.split(' ')[0].split("-")
-        dateObj1 = date(int(year), int(month), int(day))
-        year, month, day = date2.split(' ')[0].split("-")
-        dateObj2 = date(int(year), int(month), int(day))
+        d1 = date1.split(' ')
+        year, month, day = d1[0].split("-")
+        hour, minute = d1[1].split(":")
+        dateObj1 = datetime(int(year), int(
+            month), int(day), int(hour), int(minute))
+        d2 = date2.split(' ')
+        year, month, day = d2[0].split("-")
+        hour, minute = d2[1].split(":")
+        dateObj2 = datetime(int(year), int(
+            month), int(day), int(hour), int(minute))
         return dateObj1 < dateObj2
